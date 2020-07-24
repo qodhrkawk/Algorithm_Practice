eff70d076b173a8750fe1da1a4587193d9df536b
qodhrkawk <gabriel1120@naver.com>
2020-07-24 15:21:17 +0900

    7/24 3문제

---
+++ 7/24/724_3.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    bool* prime = new bool[n+1];
+    fill_n(prime,n+1,true);
+
+    for(int i = 2; i <= n/2; i++){
+
+        for(int j = 2; j <= n/i; j++){
+            prime[i*j] = false;
+        }
+        
+    }
+
+    for(int i = 2; i<= n; i++){
+        if(prime[i] == true){
+            cout<<i<<endl;
+            answer += 1;
+        }
+
+    }
+
+
+
+
+    return answer;
+}
+
+
+int main(){
+
+    cout<<solution(5)<<endl;
+
+
+    return 0;
+}
