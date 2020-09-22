65412c776bc8393a385c721415f9d53ae41c30b5
qodhrkawk <gabriel1120@naver.com>
2020-09-22 15:57:11 +0900

    22일

---
+++ 9/22/1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+using namespace std;
+
+int n,t;
+
+
+int map[1025][1025];
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+
+    cin.tie(0);
+    cout.tie(0);
+
+
+    cin>>n>>t;
+
+    for(int i = 1; i<= n;i++){
+        for(int j = 1; j<=n; j++){
+            int num;
+            cin>>num;
+
+            map[i][j] = map[i-1][j] + map[i][j-1] - map[i-1][j-1] + num;
+
+        }
+    }  
+
+
+
+    for(int test = 0; test<t; test++){
+        int x1,x2,y1,y2;
+
+        cin>>y1>>x1>>y2>>x2;
+
+        cout<<map[y2][x2] - map[y1-1][x2] - map[y2][x1-1] + map[y1-1][x1-1]<<endl;
+
+    }
+
+
+
+
+
+	return 0;
+}
