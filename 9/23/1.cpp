be5649da183edbdfc5c9c5125b9d388d4a03500a
qodhrkawk <gabriel1120@naver.com>
2020-09-23 13:14:41 +0900

    23일

---
+++ 9/23/1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+using namespace std;
+
+int n;
+const int MAX = 1001;
+
+int dp[MAX];
+
+
+
+int main() {
+
+    dp[1] = 1;
+    dp[2] = 3;
+    cin>>n;
+
+    for(int i = 3; i <= n; i++)
+        dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+
+    
+    cout<<dp[n]<<endl;
+
+
+
+
+
+
+
+
+	return 0;
+}
