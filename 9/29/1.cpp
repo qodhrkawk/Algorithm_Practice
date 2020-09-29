0a3c5dfca71636b128c39f81c66818184e56d86e
qodhrkawk <gabriel1120@naver.com>
2020-09-29 18:06:03 +0900

    29

---
+++ 9/29/1.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+
+int times[15];
+int benefits[15];
+
+int dp[20];
+
+int main(){
+
+    cin>>n;
+
+    for(int i = 1; i<=n;i++){
+        cin>>times[i];
+        cin>>benefits[i];
+    }
+
+
+
+    for(int i = 1; i <= n+1;i++){
+        dp[i] = max(dp[i],dp[i-1]);
+        if(i<=n){
+            dp[i + times[i]] = max(dp[i + times[i]],dp[i]+benefits[i]);
+        }
+    }
+
+
+    for(int i = 1; i<=n+1;i++){
+        cout<<dp[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<dp[n+1]<<endl;
+
+    return 0;
+}
