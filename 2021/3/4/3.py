676645f17a60e1b4bb88750b7534b96476b49d4b
qodhrkawk <gabriel1120@naver.com>
2021-03-04 17:07:45 +0900

    3/4 3

---
+++ 2021/3/4/3.py
@@ -0,0 +1,41 @@
+# 프로그래머스 N으로 표현
+
+
+def solution(N, number):
+    answer = 0
+
+    if N == number :
+        return 1
+
+    digits = [[],[N]]
+
+    for i in range(2,9):
+        tmp = [int(str(N)*i)]
+        for j in range(1,i//2+1) :
+            for d in digits[j] :
+                for k in digits[i-j] :
+
+                    if d!= 0 and k != 0:
+                        tmp.append(d+k)
+                        tmp.append(d*k)
+                        if d > k :
+                            tmp.append(d-k)
+                            tmp.append(d//k)
+                        else :
+                            tmp.append(k-d)
+                            if k//d != 0 :
+                                tmp.append(k//d)
+
+        digits.append(tmp)
+
+        if number in tmp :
+            return i
+
+
+
+
+
+
+    return -1
+
+print(solution(5,12))
