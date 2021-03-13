dbd9ff21e4c76fdb631a6f7a760ba227e62a497d
qodhrkawk <gabriel1120@naver.com>
2021-03-13 21:59:14 +0900

    3/13

--- 2021/3/11/1.py
+++ 2021/3/11/1.py
@@ -3,11 +3,17 @@
 
 def binarySearch(left,right,arr,val) :
 
-    for i in range(left,right+1) :
-        if arr[i] < val :
-            return i-1
+    for i in range(right,left-1,-1) :
+        if arr[i] >= val :
+            return i
+    return -1
 
-    return right-1
+    #
+    # for i in range(left,right+1) :
+    #     if arr[i] < val :
+    #         return i-1
+    #
+    # return right-1
 
 
 
@@ -20,6 +26,18 @@ def solution() :
     shape = list(map(int,input().split()))
     inputs = list(map(int,input().split()))
 
+    minimum = 300000
+
+    for idx,num in enumerate(shape) :
+        if num < minimum :
+            minimum = num
+
+        else :
+            shape[idx] = minimum
+
+    # print(shape)
+
+
 
 
     left,right = 0,len(shape)-1
@@ -30,9 +48,19 @@ def solution() :
     for idx,num in enumerate(inputs) :
 
         right = binarySearch(left,right,shape,num)
-        if right < 0 and idx < n-1:
+        print(right)
+        if right == - 1:
             print(0)
             return
+        if idx < n-1 :
+            right = right-1
+
+
+
+        # right = binarySearch(left,right,shape,num)
+        # if right < 0 and idx < n-1:
+        #     print(0)
+        #     return
      
 
 
