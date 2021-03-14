fa7a27f6db47eb5894a143ef7096853cc2f28d1b
qodhrkawk <gabriel1120@naver.com>
2021-03-14 15:39:15 +0900

    boj 10819

---
+++ 2021/3/14/1.py
@@ -0,0 +1,21 @@
+
+from itertools import permutations
+
+def solution() :
+    n = int(input())
+
+    arr = list(map(int,input().split()))
+
+
+    perm = list(permutations(arr,len(arr)))
+
+    answer = 0
+    for p in perm :
+        tmp = 0
+        for i in range(len(p)-1) :
+            tmp += abs(p[i]-p[i+1])
+        answer = max(tmp,answer)
+
+    return answer
+
+print(solution())
