5ff36d35de44a91ae6c1e3f1665dc40d2699f8fe
qodhrkawk <gabriel1120@naver.com>
2022-02-11 20:01:11 +0900

    New start

--- 2021/5/7/10.py
+++ 2021/5/7/10.py
@@ -1,6 +1,42 @@
-def solution(cookie):
-    answer = -1
 
+a,b = 0,0
+answer = ""
+
+def subSolution(s) :
+
+    global a,b,answer
+
+    if a > b :
+        if a >= 2 :
+            a -= 2
+            answer += "aa"
+        elif a == 1 :
+            a -= 1
+            answer += "a"
+    elif a == 0 and b == 0 :
+        answer += ""
+    else :
+        if b >= 2 :
+            b -= 2
+            answer += "bb"
+        elif b == 1 :
+            b -= 1
+            answer += "b"
+
+
+
+def solution(A,B):
+
+    global a,b,answer
+
+    a,b = A,B
+
+    while a > 0 and b > 0 :
+        print(a,b,answer)
+        subSolution("")
 
 
     return answer
+
+
+print(solution(2,3))
